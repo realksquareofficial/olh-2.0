effd09af2e745d41f5609c49bf30acd16da8b66a
Krish <mgkrishna2000@gmail.com>
2026-02-18 13:42:30 +0530

    firebase.js uodate

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -15,17 +15,28 @@ export const messaging = getMessaging(app);
 
 export const requestForToken = async () => {
   try {
+    let registration;
+    if ('serviceWorker' in navigator) {
+      registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
+      console.log('Service Worker registered with scope:', registration.scope);
+    } else {
+      throw new Error('Service workers are not supported in this browser.');
+    }
+
     const currentToken = await getToken(messaging, { 
-      vapidKey: 'BKCO88JCXcA-gSVfBo5fu0Qbkn0eapSQx4CgDEwVcCKXhOb_znvMaW9yY9FO-lgHjM9vkOESfbgN2huirGFSZz4'
+      vapidKey: 'BKCO88JCXcA-gSVfBo5fu0Qbkn0eapSQx4CgDEwVcCKXhOb_znvMaW9yY9FO-lgHjM9vkOESfbgN2huirGFSZz4',
+      serviceWorkerRegistration: registration
     });
+
     if (currentToken) {
+      console.log('FCM Token:', currentToken);
       return currentToken;
     } else {
       console.log('No registration token available.');
       return null;
     }
   } catch (err) {
-    console.error('An error occurred while retrieving token. ', err);
+    console.error('An error occurred while retrieving token: ', err);
     return null;
   }
 };
